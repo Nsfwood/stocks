e11fe3ce456f9fd6b4a0404eaa88356995275284
Alexander <a.rohrig@me.com>
2020-09-09 10:39:37 -0700

    Added TranslationStrings

---
+++ stocks/TranslationStrings.swift
@@ -0,0 +1,127 @@
+//
+//  UIStrings.swift
+//  stocks
+//
+//  Created by Alexander Rohrig on 7/15/20.
+//  Copyright © 2020 Alexander Rohrig. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+//let sidebar_Navigation_Bar_Title: LocalizedStringKey = "sidebar_Navigation_Bar_Title"
+//let content_NoStock_Body: LocalizedStringKey = "content_NoStock_Body"
+//let content_Navigation_Bar_Title: LocalizedStringKey = "content_Navigation_Bar_Title"
+//let ___: LocalizedStringKey = "___"
+
+enum Translation {
+    
+    static var sidebar_Navigation_Bar_Title: LocalizedStringKey {
+        return "sidebar_Navigation_Bar_Title"
+    }
+    
+    static var content_NoStock_Body: LocalizedStringKey {
+        return "content_NoStock_Body"
+    }
+    
+    static var content_Navigation_Bar_Title: LocalizedStringKey {
+        return "content_Navigation_Bar_Title"
+    }
+    
+    static var sidebar_Item_AddPrompt: LocalizedStringKey {
+        return "Press 􀁌 to add a stock to your portfolio."
+    }
+    
+    static var add_Title: LocalizedStringKey {
+        return "ADD_TITLE" // Add Stock
+    }
+    
+    static var cancel_Button: LocalizedStringKey {
+        return "CANCEL_BUTTON" // Cancel
+    }
+    
+    static var add_Search_Placeholder: LocalizedStringKey {
+        return "ADD_SEARCH_PLACEHOLDER" // Search for a stock symbol
+    }
+    
+    static var add_Search_Exchanges_Subtitle: LocalizedStringKey {
+        return "ADD_SEARCH_EXCHANGES_SUBTITLE" // Searchable exchanges: NYS, NAS, PSE, BATS, ASE
+    }
+    
+    static var add_Premium_SearchByNF: LocalizedStringKey {
+        return "ADD_PREMIUM_SEARCHBYN+F" // Upgrade to Premium for search by name and fragments.
+    }
+    
+    static var add_Result_UnableToFind: LocalizedStringKey {
+        return "ADD_RESULT_UNABLETOFIND" // Unable to find a stock with that symbol.
+    }
+    
+    static var add_Result_AddButtonTitle: LocalizedStringKey {
+        return "ADD_RESULT_BUTTON" // Add ___ to your stocks
+    }
+    
+    static var settings_Title: LocalizedStringKey {
+        return "SETTINGS_TITLE"
+    }
+    
+    static var done_Button: LocalizedStringKey {
+        return "DONE_BUTTON"
+    }
+    
+    static var content_Premium_MoreThan5Filter: LocalizedStringKey {
+        return "CONTENT_PREMIUM_5+F"
+    }
+    
+    static var upgrade_Button: LocalizedStringKey {
+        return "UPGRADE_BUTTON"
+    }
+    
+    static var PROVIDED_BY_IEX: LocalizedStringKey {
+        return "PROVIDED_BY_IEX"
+    }
+    
+    static var FILTER_X_TITLE: LocalizedStringKey {
+        return "FILTER_X_TITLE"
+    }
+    
+    static var FILTER_X_SUBTITLE: LocalizedStringKey {
+        return "FILTER_X_SUBTITLE"
+    }
+    
+    static var FILTER_X_START: LocalizedStringKey {
+        return "FILTER_X_START"
+    }
+    
+    static var FILTER_X_END: LocalizedStringKey {
+        return "FILTER_X_END"
+    }
+    
+    static var FILTER_X_INCLUDE: LocalizedStringKey {
+        return "FILTER_X_INCLUDE"
+    }
+    
+    static var FILTER_X_CLEAR: LocalizedStringKey {
+        return "FILTER_X_CLEAR"
+    }
+    
+    static var FILTER_Y_TITLE: LocalizedStringKey {
+        return "FILTER_Y_TITLE"
+    }
+    
+    static var FILTER_Y_START: LocalizedStringKey {
+        return "FILTER_Y_START"
+    }
+    
+    static var FILTER_Y_END: LocalizedStringKey {
+        return "FILTER_Y_END"
+    }
+    
+    static var FILTER_Y_CLEAR: LocalizedStringKey {
+        return "FILTER_Y_CLEAR"
+    }
+    
+    static var ___: LocalizedStringKey {
+        return "___"
+    }
+    
+}
